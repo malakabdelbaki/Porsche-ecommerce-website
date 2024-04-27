e7838b7fa1d9591580605d46ba47e1b629c15a21
ro ro <roaaradwan2020@gmail.com>
2024-04-27 22:22:31 +0300

    implemented add to cart functionality

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,6 +1,34 @@
-const addToCart = async (req,res) => {
+const Customer = require('../models/customer');
+const Product = require('../models/product');
 
-}
+const addToCart = async (req,res) => {
+    try {
+        const customerId = req.body.customerID; 
+        const productId = req.body.productId; 
+    
+        
+        const customer = await Customer.findById(customerId);
+        const product = await Product.findById(productId);
+    
+        if (!customer || !product) {
+            return res.status(404).json({ message: 'Customer not found.' });
+          }
+        else if(!product){
+            return res.status(404).json({ message: 'Product not found.' });
+        }
+      
+          customer.cart.push(product);
+          await customer.save();
+      
+          res.status(200).json({ message: 'Product added to cart successfully.' });
+        } catch (error) {
+          console.error(error);
+          res.status(500).json({ message: 'Error adding product to cart' });
+        }
+      }
+
+
+
 
 const removeFromCart = async (req,res) => {
 
