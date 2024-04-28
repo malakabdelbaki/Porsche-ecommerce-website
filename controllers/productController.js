9b1f5ea9a870e7139bdbed8d5b33f981a4d3434a
MarinaaEmil <marina.emil2003@gmail.com>
2024-04-28 14:58:31 +0300

    'implemented delete product'

--- controllers/productController.js
+++ controllers/productController.js
@@ -16,6 +16,15 @@ const updateProduct = async (req,res) => {
 }
 
 const deleteProduct = async (req,res) => {
+    try {
+        const result = await Product.findByIdAndDelete(req.params.id);
+        if (!result) {
+          return res.status(404).json({ message: 'No product found with that ID' });
+        }
+        res.status(200).json({ message: 'Product deleted successfully', productId: result._id });
+      } catch (error) {
+        res.status(500).json({ message: 'Failed to delete product' });
+      }
 
 }
 
