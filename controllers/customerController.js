2e2d2ffd17d03679dfcc716c6a795ad75d6964ed
yousefnegmeldin <yousefnegm@gmail.com>
2024-04-28 14:13:24 +0300

    added removefromcart functionality

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,9 +1,29 @@
-const addToCart = async (req,res) => {
+const addToCart = async (req, res) => {};
 
-}
+const removeFromCart = async (req, res) => {
+  try {
+    const customerID = req.body.customerID;
+    const productID = req.body.productID;
 
-const removeFromCart = async (req,res) => {
+    const customer = await Customer.findById(customerID);
+    const product = await Product.findById(productID);
 
-}
+    if (!customer || !product) {
+      return res.status(404).json({ message: "Customer not found." });
+    } else if (!product) {
+      return res.status(404).json({ message: "Product not found." });
+    }
 
-module.exports = {addToCart,removeFromCart}
+    customer.cart = customer.cart.filter(
+      (product) => product._id !== productID
+    );
+    await customer.save();
+
+    res.status(200).json({ message: "Product deleted." });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error deleting product from cart" });
+  }
+};
+
+module.exports = { addToCart, removeFromCart };
