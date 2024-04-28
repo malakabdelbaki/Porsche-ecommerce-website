685aea58987aeff151fbeb6f3de030c8eb88d679
yousefnegmeldin <yousefnegm@gmail.com>
2024-04-28 17:50:26 +0300

    fixed module export

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,49 +1,43 @@
-const Admin = require('../models/admin');
-const Product = require('../models/product');
+const Admin = require("../models/admin");
+const Product = require("../models/product");
 
+const addProduct = async (req, res) => {
+  try {
+    const { name, price, description, category, production_year } = req.body;
 
-const addProduct = async (req,res) => {
-    try {
-        
-        const { name, price, description, category, production_year } = req.body;
+    const newProduct = new Product({
+      name,
+      price,
+      description,
+      category,
+      production_year,
+    });
 
-        const newProduct = new Product({
-          name,
-          price,
-          description,
-          category,
-          production_year
-        });
-    
-        await newProduct.save();
-    
-        res.status(201).json({ message: 'Product added successfully.', product: newProduct });
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Error' });
-      }
-    }
-    
-    module.exports = addProduct;
+    await newProduct.save();
 
+    res
+      .status(201)
+      .json({ message: "Product added successfully.", product: newProduct });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error" });
+  }
+};
 
+module.exports = addProduct;
 
+const getProducts = async (req, res) => {};
 
-const getProducts = async (req,res) => {
+const getProductById = async (req, res) => {};
 
-}
+const updateProduct = async (req, res) => {};
 
-const getProductById = async (req,res) => {
+const deleteProduct = async (req, res) => {};
 
-
-}
-
-const updateProduct = async (req,res) => {
-
-}
-
-const deleteProduct = async (req,res) => {
-
-}
-
-module.exports = {getProducts,getProductById,createProduct,updateProduct,deleteProduct}
+module.exports = {
+  getProducts,
+  getProductById,
+  addProduct,
+  updateProduct,
+  deleteProduct,
+};
